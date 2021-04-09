1bac6f3ae9c6f947bf84aa81e9d9865d8b1dc679
Jim Burnham <jim.the.steam.clown@gmail.com>
2021-04-09 10:25:16 -0700

    Update pi_blink-LED.py

--- pi_blink-LED.py
+++ pi_blink-LED.py
@@ -6,11 +6,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 
-for i in range(20)
-  print "LED on"
+for i in range(20):
+  print("LED on")
   GPIO.output(18,GPIO.HIGH)
   time.sleep(1)
-  print "LED off"
+  print("LED off")
   GPIO.output(18,GPIO.LOW)
   time.sleep(1)
 print('Done')
