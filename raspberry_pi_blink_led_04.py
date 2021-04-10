8153acc282ba5507c703913a0f26de2b7175f96d
Jim Burnham <jim.the.steam.clown@gmail.com>
2021-04-10 12:19:07 -0700

    Create raspberry_pi_blink_led_04.py

---
+++ raspberry_pi_blink_led_04.py
@@ -0,0 +1,17 @@
+#
+import RPi.GPIO as GPIO
+import time
+
+blink_led = 5
+GPIO.setmode(GPIO.BOARD) # Note This is specifying the physical pins on the Raspberry Pi Header 
+GPIO.setwarnings(False)
+GPIO.setup(blink_led,GPIO.OUT)
+
+for i in range(25):
+  print("LED on - cycle",i+1)
+  GPIO.output(blink_led,GPIO.HIGH)
+  time.sleep(.5)
+  print("LED off")
+  GPIO.output(blink_led,GPIO.LOW)
+  time.sleep(.5)
+print('Done')
