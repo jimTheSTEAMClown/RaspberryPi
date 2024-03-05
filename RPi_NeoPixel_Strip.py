aa44f8c34c1120ece1048efb3f4e1f3dfa4721c2
Jim Burnham <jim.the.steam.clown@gmail.com>
2024-03-04 19:55:44 -0800

    Update RPi_NeoPixel_Strip.py
    adding Neopixel Strip test function

--- RPi_NeoPixel_Strip.py
+++ RPi_NeoPixel_Strip.py
@@ -1,32 +1,44 @@
 # ============================================================================
-# Source: www.steamclown.org
-# GitHub: <if you don't have a github link, then delete this line>
-# Hacker: <your name - NickName, <more nouns to describe how awesome you are Engineer, Maker, > 
+# Source: STEAM Clown - www.steamclown.org 
+# GitHub: https://raw.githubusercontent.com/jimTheSTEAMClown/RaspberryPi/master/RPi_NeoPixel_Strip.py
+# Hacker: Jim Burnham - STEAM Clown, Engineer, Teacher, Maker, Propmaster & Adrenologist  
 # This example code is licensed under the CC BY-NC-SA 4.0, GNU GPL and EUPL
 # https://creativecommons.org/licenses/by-nc-sa/4.0/
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 # https://eupl.eu/
-# Program/Design Name:		<nameYourProgram.py>
-# Description:    This is a template for python programs... change this for your 
-# program description... should be 1-4 lines
+# Program/Design Name:		RPi_NeoPixel_Strip.py
+# Description:    This is a program to drive a set of Neopixel design with functions, 
+# that can be called with external parameters that are read using a post/get feature.
+# program description:
+# 1) Read user input from the data read in the post/get code
+# 2) Select NeoPixel function and pattern
+# 3) Drive a 101 NeoPixel strip
 # Dependencies:   python3
 #   Inputs: <list any expected user input here>
 #   Outputs: <list any expected program output here>
 # Revision: 
-#  Revision 0.03 - Updated 06/09/2020 <add your update description here>
-#  Revision 0.02 - Updated 06/09/2020 added 'input / output' to the Dependencies section 
-#  Revision 0.01 - Created 01/20/2018
+#  Revision 0.02 - Updated 03/04/2024 added Raspberry Pi Neopixel libraries and features 
+#  Revision 0.01 - Created 03/04/2024
 # Additional Comments: 
 # 
 # ============================================================================
+import board
+import time
+import neopixel
+pixels = neopixel.NeoPixel(board.D18, 30) # strip of 20 for testing.  Planned Strip has 101 Neopixels
+# ============================================================================
+def setup_test():
+   # This function tests the Neopixel strip
+   pixels[0] = (255, 0, 0)
+   pixels.fill((0, 255, 0))
+   
+
 
 def main():
    # This is the main function.  all your main code goes here.  
    # You can call other functions from here
-   x=2
-   y=6
-   z=x+y
-   print(z)
+   setup_test()
+   
    # All done
 
 # This is the call to the Function main().  You should always 
