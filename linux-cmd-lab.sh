ae19b59f1cf3575f526fb7ca1eb0222c07407105
Jim Burnham <jim.the.steam.clown@gmail.com>
2024-03-10 12:00:08 -0700

    Create linux-cmd-lab.sh

---
+++ linux-cmd-lab.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+# ============================================================================
+# shell script to create command line lab structure
+# Source: STEAM Clown - www.steamclown.org 
+# GitHub: https://github.com/jimTheSTEAMClown/Linux
+# Hacker: Jim Burnham - STEAM Clown, Engineer, Maker, Propmaster & Adrenologist 
+# This example code is licensed under the CC BY-NC-SA 4.0, GNU GPL and EUPL
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+# https://eupl.eu/
+# Program/Design Name:		linux-cmd-lab.sh
+# Description:    shell script to WGET the files to setup and update a clean Raspberry Pi Image 
+# Dependencies:   
+# Revision: 
+#  
+#  Revision 0.02 - Updated 02/21/2024 for SVCTE Mechatronics Class
+#  Revision 0.01 - Created 02/21/2024
+# Additional Comments:
+# ============================================================================
+# Make sure you have the linuxCommandLab directory
+# cd ~
+# mkdir linuxCommandLab
+# sudo wget -O ./linuxCommandLab/linux-cmd-lab.sh https://raw.githubusercontent.com/jimTheSTEAMClown/RaspberryPi/master/New_64-Bit_Pi_Build_WGET.sh
+# sudo chmod 755 ./linuxCommandLab/linux-cmd-lab.sh
+# sudo cat ./linuxCommandLab/linux-cmd-lab.sh
+
+
+
