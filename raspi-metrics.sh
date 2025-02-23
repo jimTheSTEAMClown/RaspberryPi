ad5a950907e6a7e69d0aba9edce622252de1b81c
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-02-22 16:31:03 -0800

    Create raspi-metrics.sh
    see: https://downey.io/blog/raspberry-pi-3-heat-sink-comparison/

---
+++ raspi-metrics.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# https://downey.io/blog/raspberry-pi-3-heat-sink-comparison/
+set -eu -o pipefail
+
+SECONDS=${1:-60}
+
+iso8601_timestamp() {
+  date -u +%FT%TZ
+}
+
+current_cpu_temp_in_c() {
+  /opt/vc/bin/vcgencmd measure_temp | cut -d"=" -f2 | cut -d"'" -f1
+}
+
+current_cpu_clock_speed() {
+  cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
+}
+
+for i in `seq 1 $SECONDS`; do
+ echo "$(iso8601_timestamp), $(current_cpu_temp_in_c), $(current_cpu_clock_speed)"
+ sleep 1
+done
