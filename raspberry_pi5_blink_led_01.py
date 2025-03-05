35f449da2708b638f2598010317b32547f72a63c
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-03-05 10:47:57 -0800

    Update raspberry_pi5_blink_led_01.py

--- raspberry_pi5_blink_led_01.py
+++ raspberry_pi5_blink_led_01.py
@@ -1,15 +1,15 @@
-#
 # import gpiozero
 from gpiozero import LED
 import time
 
-blink_led = LED(3) # BCM GPIO3 = BOARD 5
+# Using BCM GPIO3 I/O on BOARD pin 5
+red_led = LED(3) # BCM GPIO3 = BOARD 5
 
 while(True):
   print("LED on")
-  blink_led.on()
+  red_led.on()
   time.sleep(1)
   print("LED off")
-  blink_led.off()
+  red_led.off()
   time.sleep(1)
 print('Done')
