60184e38fbaa1fb3a861c49bcb791f7bda1acd06
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-03-06 13:36:48 -0800

    Update raspberry_pi5_switch_led_01.py

--- raspberry_pi5_switch_led_01.py
+++ raspberry_pi5_switch_led_01.py
@@ -7,7 +7,7 @@ import time
 red_led = LED(3) # BCM GPIO3 = BOARD 5
 red_button = Button(7,pull_up = True,bounce_time= None) 
 while(True):
-  if red_button.is_pressed():
+  if red_button.is_pressed:
     print("Switch is pressed")
   else:
     print("Switch is not pressed")
