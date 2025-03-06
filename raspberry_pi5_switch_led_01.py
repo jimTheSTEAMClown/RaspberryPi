09d5852abaa38176614353c478670b381e141516
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-03-06 07:09:37 -0800

    Create raspberry_pi5_switch_led_01.py

---
+++ raspberry_pi5_switch_led_01.py
@@ -0,0 +1,16 @@
+# import gpiozero
+from gpiozero import LED
+from gpiozero import Button
+import time
+
+# Using BCM GPIO3 I/O on BOARD pin 5
+red_led = LED(3) # BCM GPIO3 = BOARD 5
+red_button = Button(7,pull_up = True,bounce_time= None) 
+while(True):
+  if red_button.is_pressed():
+    print("Switch is pressed")
+  else:
+    print("Switch is not pressed")
+  # Add a small delay if needed to avoid excessive CPU usage
+  time.sleep(0.1)
+
